77fb9283bded0b8af9ebb1801f1c738fabf23a80
Cy83rr <cy83rr+github@gmail.com>
2016-01-17 16:31:24 +0100

    added template script

---
+++ IV_fitter.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as pyplot
+import numpy
+import scipy.optimize as optimize
+
+voltages = []
+currents = []
+currentsErr= []
+rescale = 1.0
+
+def readData(filename):
+    with open(filename) as dataFile:
+        data = numpy.loadtxt(dataFile, skiprows=1)
+        global voltages
+        global currents
+        global currentsErr
+        global rescale
+        voltages = data[:,0]
+        currents = data[:,1]
+        currentsErr = data[:,2]/1000
+        rescale = currents.max()
+
+        print(voltages)
+        print(currents)
+        print(currentsErr)
+        voltages = voltages/rescale
+
+def chisqfunction(a_b):
+    a, b = a_b
+    model = a + b*currents
+    chisq = numpy.sum(((voltages- model)/currentsErr)**2)
+    return chisq
+
+x0 = numpy.array([0,0])
+
+readData('testData.txt')
+result =  optimize.minimize(chisqfunction, x0)
+print(result)
+assert result.success==True
+
+pyplot.scatter(currents, voltages*rescale)
+a,b=result.x*rescale
+print(a,b)
+pyplot.plot(currents, a+b*currents)
+pyplot.savefig('firstplot.png')
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IV-fitter
 Python script used for fitting models to IV data
+Usage: put the files in the folder containg IV data in TSV format and run IV_fitter.py
