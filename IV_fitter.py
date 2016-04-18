f056bcfe75e242a1ece369f012ac6814abef4ead
Adam Łoszyn <adam.loszyn@xtb.com>
2016-04-18 09:55:37 +0200

    Updated README, changed initial parameter values and model

--- IV_fitter.py
+++ IV_fitter.py
@@ -12,6 +12,9 @@ import pandas
 # TODO: decide, how it should look like user-wise
 # TODO: add labels to chart
 
+# TODO write all necessary dependencies or smth like gradle in java -
+# TODO http://docs.activestate.com/activepython/3.2/diveintopython3/html/packaging.html
+
 def readData(filename):
     with open(filename) as dataFile:
         data = pandas.read_csv(dataFile, sep='\t', decimal=',').values
@@ -34,16 +37,16 @@ def model(rcontact, n0, vdirac, mobility, voltages):
     # sample length/width, unitless
     sampleDimension=2
     # vaccum permittivity
-    epsilonZero=8.854187817*10**-12
+    epsilonZero=8.854187817*1e-12
     # relative electric permittivity of the substrate: 11.68 SI, 3.9 SIO2
     epsilon=3.9
     # capacitance of the oxydant on the gate, per unit of surface area
     cox=epsilon*epsilonZero/(285*1e-9) / 1e4 # divided by 1e4 to convert to square centimeters
     # eletron charge
-    echarge=1.6021766208*(10**-19)
+    echarge=1.6021766208*(1e-19)
 
-    return 2 * rcontact + \
-            (sampleDimension / (numpy.sqrt(n0**2 + (cox * (voltages - vdirac) / echarge)**2) * echarge * mobility))
+    return 2 * rcontact*1e7 + \
+            (sampleDimension / (numpy.sqrt((n0*1e9)**2 + (cox * (voltages - vdirac) / echarge)**2) * echarge * mobility*1e3))
 
 def chisqfunction(params, voltagesData, resistanceData, currentsData, currentsErr):
     rcontact = params['rcontact'].value
@@ -60,10 +63,10 @@ def chisqfunction(params, voltagesData, resistanceData, currentsData, currentsEr
     return numpy.array(((resistanceData - predicted)/predicted))
 # set initial parameters with bounds
 initialParameters = lmfit.Parameters()
-initialParameters.add('mobility', value=4000, min=1e2, max=2e5)
-initialParameters.add('rcontact', value=1E7, min=1e2)
-initialParameters.add('n0', value=1E9, min=1e3)
-initialParameters.add('vdirac', value=60, min=55, max=65)
+initialParameters.add('mobility', value=1, min=1e-1, max=100)
+initialParameters.add('rcontact', value=3, min=1e-4)
+initialParameters.add('n0', value=1.2, min=1e-3)
+initialParameters.add('vdirac', value=60, min=58, max=65)
 
 voltages, currents, currentsErr, resistance = readData('testData.txt')
 # TODO: use a minimizer, might help counting stderr and correl and is (?) needed for correl charts
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # IV-fitter
-Python script used for fitting models to IV data
-Usage: put the files in the folder containg IV data in TSV format and run IV_fitter.py
+Python script used for fitting models to IV data.
+**Features:** Fitting a specific model to provided data, creating charts, computing linear correlations between fitting parameters and (in progress) creating correlation charts
+**Usage:** Put the files in the folder containg IV data in TSV format and run IV_fitter.py
