be6f62abe3b2417a8eefca7e32e8cfc69db97bf7
Cy83rr <cy83rr+github@gmail.com>
2016-05-22 14:38:03 +0200

    modified README, extracted fitting to a function, added some user prompts

--- IV_fitter.py
+++ IV_fitter.py
@@ -1,7 +1,11 @@
+import glob
+import os
+
 import lmfit
 import matplotlib.pyplot as pyplot
 import numpy
 import pandas
+
 
 # TODO: remember to properly cite lmfit library
 # TODO: including current error - weighted least squares?
@@ -11,6 +15,9 @@ import pandas
 # TODO: make it a script
 # TODO: decide, how it should look like user-wise
 # TODO: add labels to chart
+# TODO: remember to cite properly matplotlib and other libs!
+
+#resistanceErr = numpy.array()
 
 # TODO write all necessary dependencies or smth like gradle in java -
 # TODO http://docs.activestate.com/activepython/3.2/diveintopython3/html/packaging.html
@@ -57,34 +64,66 @@ def chisqfunction(params, voltagesData, resistanceData, currentsData, currentsEr
 
     # TODO: how to include those errors?
     # resistance error
+    #global resitanceErr
     resitanceErr = numpy.array(abs((voltagesData/(currentsData**2))*currentsErr))
 
     predicted = numpy.array(model(rcontact, n0, vdirac, mobility, voltagesData))
     return numpy.array(((resistanceData - predicted)/predicted))
+
+def plotFigures(initialParameters, fileName, resultPath):
+    voltages, currents, currentsErr, resistance = readData(fileName)
+    # TODO: use a minimizer, might help counting stderr and correl and is (?) needed for correl charts
+    minimizer = lmfit.Minimizer(chisqfunction, initialParameters,
+                                fcn_args=(voltages, resistance, currents, currentsErr))
+    result = minimizer.leastsq()
+
+    print(result)
+    # TODO: change the order of commands to draw - avoid needless redrawing
+    ci, trace = lmfit.conf_interval(minimizer, result, sigmas=[0.68, 0.95], trace=True, verbose=False)
+    x, y, prob = trace['mobility']['mobility'], trace['mobility']['vdirac'], trace['mobility']['prob']
+    x2, y2, prob2 = trace['vdirac']['vdirac'], trace['vdirac']['mobility'], trace['vdirac']['prob']
+    fig1 = pyplot.figure(0)
+    # ax1 = fig1.add_subplot(111)
+    pyplot.scatter(x, y, c=prob)
+    pyplot.scatter(x2, y2, c=prob2)
+    pyplot.xlabel('mobility')
+    pyplot.ylabel('vdirac')
+    pyplot.savefig('mob-n0.png')
+
+    fittedParameters = result.params
+    fittedData = resistance + result.residual
+
+    print(lmfit.fit_report(result, min_correl=0.1))
+
+    fig2 = pyplot.figure(1)
+    # ax2 = fig2.add_subplot(111)
+    scatter = pyplot.scatter(voltages, resistance)
+    line, = pyplot.plot(voltages, fittedData, color='red')
+    pyplot.xlabel('Gate voltage [ V ]')
+    pyplot.ylabel('Resistance [ \u2126 ]')
+    pyplot.text(0.95, 0.1, 'Sample dimension: 2')
+    pyplot.text(3, 2, 'V_DS: 10 V')
+    # ax2.errorbar(voltages, resistance, resistanceErr)
+    pyplot.legend((scatter, line), ['Data', 'Fit'], loc="best")
+    # TODO: save additional information on the chart: sample dimensions, source-drain voltage
+    resultName=os.path.splitext(fileName)[0]
+    pyplot.savefig(os.path.join(resultPath, resultName))
+
+
+
 # set initial parameters with bounds
 initialParameters = lmfit.Parameters()
-initialParameters.add('mobility', value=1, min=1e-1, max=100)
-initialParameters.add('rcontact', value=3, min=1e-4)
+initialParameters.add('mobility', value=2, min=1e-1, max=100)
+initialParameters.add('rcontact', value=4, min=1e-4)
 initialParameters.add('n0', value=1.2, min=1e-3)
-initialParameters.add('vdirac', value=60, min=58, max=65)
+initialParameters.add('vdirac', value=60, min=55, max=65)
 
-voltages, currents, currentsErr, resistance = readData('testData.txt')
-# TODO: use a minimizer, might help counting stderr and correl and is (?) needed for correl charts
-minimizer = lmfit.Minimizer(chisqfunction, initialParameters, fcn_args=(voltages, resistance, currents, currentsErr))
-result = minimizer.leastsq()
+filePath=input("Write the path to data files (default: current directory): ")
+resultPath=input("Write the path where the results will be saved(default: data directory): ")
 
-print(result)
+if not resultPath.strip():
+    resultPath=filePath
 
-#ci, trace = lmfit.conf_interval(minimizer, result, sigmas=[0.68,0.95], trace=True, verbose=False)
-#pyplot.plot(trace)
-#pyplot.savefig('trace.png')
-#pyplot.new_figure_manager
-pyplot.scatter(voltages, resistance)
+for infile in glob.glob( os.path.join(filePath, '*.txt')):
+    plotFigures(initialParameters, infile, resultPath)
 
-fittedParameters = result.params
-
-fittedData = resistance + result.residual
-
-print(lmfit.fit_report(result, min_correl=0.1))
-pyplot.plot(voltages, fittedData)
-pyplot.savefig('plot2.png')
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # IV-fitter
-Python script used for fitting models to IV data.
+Python script used for fitting models to IV data
+**Instalation:** Script is written using python3. Other used libraries: 
 **Features:** Fitting a specific model to provided data, creating charts, computing linear correlations between fitting parameters and (in progress) creating correlation charts
 **Usage:** Put the files in the folder containg IV data in TSV format and run IV_fitter.py
