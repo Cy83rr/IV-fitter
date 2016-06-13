eee42a089301e58eb8388aff0acf9923b1286b20
Cy83rr <cy83rr+github@gmail.com>
2016-06-13 20:18:49 +0200

    added comments, rescaled values, updated README

--- IV_fitter.py
+++ IV_fitter.py
@@ -11,16 +11,14 @@ import pandas
 # TODO: including current error - weighted least squares?
 # TODO: write doc with necessary libraries to run program
 # TODO: correlation charts
-# TODO: fit parameters have huge errors - precision problem?
 # TODO: make it a script
 # TODO: add labels to chart
 # TODO: remember to cite properly matplotlib and other libs!
 
-# resistanceErr = numpy.array()
-
 # TODO write all necessary dependencies or smth like gradle in java -
 # TODO http://docs.activestate.com/activepython/3.2/diveintopython3/html/packaging.html
 
+# read data from a file and cuts it off at a specific place
 def readData(filename):
     with open(filename) as dataFile:
         data = pandas.read_csv(dataFile, sep='\t', decimal=',').values
@@ -36,24 +34,30 @@ def readData(filename):
         currents = numpy.array(data[cutOffIndex:, 1])
         currentsErr = numpy.array(data[cutOffIndex:, 2])
 
-        resistance = numpy.array([a / b for a, b in zip(abs(voltages), currents)])  # to make kiloOhms
-        return voltages, currents, currentsErr, resistance
-
-def model(rcontact, n0, vdirac, mobility, voltages):
+        resistance = numpy.array([a / b for a, b in zip(abs(voltages), currents)])
+        # to make kiloOhms
+        scaledResistance = resistance/1e6
+        return voltages, currents, currentsErr, scaledResistance
+# TODO check units!
+def model(voltages, rcontact, n0, vdirac, mobility):
     # sample length/width, unitless
-    sampleDimension=2
-    # vaccum permittivity
-    epsilonZero=8.854187817*1e-12
+    sampleDimension = 2
+    # vaccum permittivity in [F/m]
+    epsilonZero = 8.854187817*1e-12 #*(1e-2)  to make F/cm from F/m
     # relative electric permittivity of the substrate: 11.68 SI, 3.9 SIO2
     epsilon=3.9
-    # capacitance of the oxydant on the gate, per unit of surface area
-    cox=epsilon*epsilonZero/(285*1e-9) / 1e4  # divided by 1e4 to convert to square centimeters
-    # eletron charge
-    echarge=1.6021766208*1e-19
+    # oxidant thickness in [m]
+    tox = 285*1e-9 #*1e2 to make [cm]
+    # capacitance of the oxidant on the gate, per unit of surface area
+    cox = epsilon * epsilonZero / tox  # divided by 1e4 to convert to square centimeters
+    # electron charge in [C]
+    echarge = 1.6021766208*1e-19
 
-    return 2 * rcontact*1e7 + \
+    # 1e4 and 1e-4 is for converting to square centimeters
+    return 2 * rcontact + \
         (sampleDimension /
-            (numpy.sqrt((n0*1e9)**2 + (cox * (voltages - vdirac) / echarge)**2) * echarge * mobility*1e3))
+            (numpy.sqrt((n0*1e10*1e4)**2 + (cox * (voltages - vdirac) / echarge)**2) * echarge * mobility*1e2*1e-4))\
+        / 1e6  # to make kiloOhms
 
 def chisqfunction(params, voltagesData, resistanceData, currentsData, currentsErr):
     rcontact = params['rcontact'].value
@@ -67,8 +71,8 @@ def chisqfunction(params, voltagesData, resistanceData, currentsData, currentsEr
     #  global resitanceErr
     resitanceErr = numpy.array(abs((voltagesData/(currentsData**2))*currentsErr))
 
-    predicted = numpy.array(model(rcontact, n0, vdirac, mobility, voltagesData))
-    return numpy.array(((resistanceData - predicted)/predicted))
+    predicted = numpy.array(model(voltagesData, rcontact, n0, vdirac, mobility))
+    return numpy.array((predicted - resistanceData) / numpy.sqrt(predicted))
 
 def plotFigures(initialParameters, fileName, resultPath):
 
@@ -78,6 +82,9 @@ def plotFigures(initialParameters, fileName, resultPath):
     resultName=os.path.split(os.path.splitext(fileName)[0])[1]
     voltages, currents, currentsErr, resistance = readData(fileName)
     # fitting to data using lestsq method
+    #gmod = lmfit.Model(model)
+    #result = gmod.fit(resistance, voltages=voltages, params=initialParameters)
+
     minimizer = lmfit.Minimizer(chisqfunction, initialParameters,
                                 fcn_args=(voltages, resistance, currents, currentsErr))
     result = minimizer.leastsq()
@@ -90,27 +97,29 @@ def plotFigures(initialParameters, fileName, resultPath):
         fitResult.write(lmfit.fit_report(result))
 
     # TODO: change the order of commands to draw - avoid needless redrawing
-    ci, trace = lmfit.conf_interval(minimizer, result, sigmas=[0.68, 0.95], trace=True, verbose=False)
-    x, y, prob = trace['mobility']['mobility'], trace['mobility']['vdirac'], trace['mobility']['prob']
-    x2, y2, prob2 = trace['vdirac']['vdirac'], trace['vdirac']['mobility'], trace['vdirac']['prob']
-    fig1 = pyplot.figure(0)
+    #ci, trace = lmfit.conf_interval(minimizer, result, sigmas=[0.68, 0.95], trace=True, verbose=False)
+    #x, y, prob = trace['mobility']['mobility'], trace['mobility']['vdirac'], trace['mobility']['prob']
+    #x2, y2, prob2 = trace['vdirac']['vdirac'], trace['vdirac']['mobility'], trace['vdirac']['prob']
+    #fig1 = pyplot.figure(0)
     # ax1 = fig1.add_subplot(111)
-    pyplot.scatter(x, y, c=prob)
-    pyplot.scatter(x2, y2, c=prob2)
-    pyplot.xlabel('mobility')
-    pyplot.ylabel('vdirac')
-    pyplot.savefig(os.path.join(resultPath, resultName+'_mob-n0.png'))
-
-    fittedData = resistance + result.residual
+    #pyplot.scatter(x, y, c=prob)
+    #pyplot.scatter(x2, y2, c=prob2)
+    #pyplot.xlabel('mobility')
+    #pyplot.ylabel('vdirac')
+    #pyplot.savefig(os.path.join(resultPath, resultName+'_mob-n0.png'))
 
     print(lmfit.fit_report(result, min_correl=0.1))
-
+    fittedParameters = result.params
+    fittedData = model(voltages, fittedParameters.get('rcontact').value,
+                       fittedParameters.get('n0').value,
+                       fittedParameters.get('vdirac').value,
+                       fittedParameters.get('rcontact').value)
     fig2 = pyplot.figure(1)
     # ax2 = fig2.add_subplot(111)
     scatter = pyplot.scatter(voltages, resistance)
     line, = pyplot.plot(voltages, fittedData, color='red')
     pyplot.xlabel('Gate voltage [ V ]')
-    pyplot.ylabel('Resistance [ \u2126 ]')
+    pyplot.ylabel('Resistance [ k\u2126 ]')
     pyplot.text(0.95, 0.1, 'Sample dimension: 2')
     pyplot.text(3, 2, 'V_DS: 10 V')
     # ax2.errorbar(voltages, resistance, resistanceErr)
@@ -118,14 +127,13 @@ def plotFigures(initialParameters, fileName, resultPath):
     # TODO: save additional information on the chart: sample dimensions, source-drain voltage
     pyplot.savefig(os.path.join(resultPath, resultName))
 
-
-
 # set initial parameters with bounds
 initialParameters = lmfit.Parameters()
-initialParameters.add('mobility', value=2, min=0, max=100)
-initialParameters.add('rcontact', value=4, min=0)
-initialParameters.add('n0', value=1.2, min=0)
+initialParameters.add('mobility', value=20, min=1, max=2e2)  # value times 1e2
+initialParameters.add('rcontact', value=0.01, min=0, max=1e1)  # value times 1e6
+initialParameters.add('n0', value=10, min=0.1, max=1e3)  # value times 1e10
 initialParameters.add('vdirac', value=60, min=55, max=65)
+
 
 filePath=input("Write the path to data files (default: current directory): ")
 resultPath=input("Write the path where the results will be saved(default: data directory/results): ")
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # IV-fitter
 Python script used for fitting models to IV data
+Opis warunków, w których było testowane oprogramowanie
+**Data format** opis formatu danych przetwarzanych i wynikowych
 **Instalation:** Script is written using python3. Other used libraries: 
 **Features:** Fitting a specific model to provided data, creating charts, computing linear correlations between fitting parameters and (in progress) creating correlation charts
 **Usage:** Put the files in the folder containg IV data in TSV format and run IV_fitter.py
